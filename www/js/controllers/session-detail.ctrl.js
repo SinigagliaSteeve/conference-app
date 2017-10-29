fba02facb5959aa30c9ed80aa9dccd08a1ee6a0b
Sinigaglia Steeve <steeve.sinigaglia@laposte.net>
2017-10-29 19:33:30 +0100

    cordova photo save in innerDB + open via modal

--- www/js/controllers/session-detail.ctrl.js
+++ www/js/controllers/session-detail.ctrl.js
@@ -4,7 +4,7 @@
     angular.module('conferenceApp')
         .controller('SessionDetailCtrl', SessionDetailCtrl);
 
-    function SessionDetailCtrl($scope, $stateParams, SessionsSrv, NotesSrv, SpeakersSrv) {
+    function SessionDetailCtrl($scope, $ionicModal, $stateParams, $cordovaCamera, $cordovaFile, SessionsSrv, NotesSrv, SpeakersSrv, uuid2) {
         var vm = this;
         vm.speakers = [];
         vm.note = {};
@@ -24,8 +24,8 @@
                     })
 
                     NotesSrv.getForSessionId($stateParams.sessionId).then(note => {
-                            vm.note = note ? note : {};
-                            vm.note.sessionId = $stateParams.sessionId;
+                        vm.note = note ? note : { pictures: [] };
+                        vm.note.sessionId = $stateParams.sessionId;
                     })
                     //get notes for this session
                     return session;
@@ -34,6 +34,45 @@
 
         vm.saveNote = function () {
             NotesSrv.save(vm.note);
+        }
+
+        vm.takePhoto = function () {
+            var options = {
+                quality: 100,
+                destinationType: Camera.DestinationType.FILE_URI,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: false,
+                correctOrientation: true,                
+                encodingType: Camera.EncodingType.JPEG,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: true
+            };
+
+            $cordovaCamera.getPicture(options).then(imageData => {
+                vm.note.pictures.push(imageData);
+                NotesSrv.save(vm.note).then(success => {
+                    // $scope.$apply();
+                })
+            },
+                err => {
+
+                });
+        }
+
+        $ionicModal.fromTemplateUrl('modal.html', function (modal) {
+            $scope.gridModal = modal;
+        }, {
+                scope: $scope,
+                animation: 'slide-in-up'
+            })
+        $scope.openModal = function (data) {
+            $scope.inspectionItem = data;
+            $scope.gridModal.show();
+            console.log(data);
+            $scope.pictureToDisplay = data;
+        }
+        $scope.closeModal = function () {
+            $scope.gridModal.hide();
         }
 
 
